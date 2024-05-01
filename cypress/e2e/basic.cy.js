0700dd24b87b26d981e5457925ecff0b824ee94b
dyl <12650315+dylandlr@users.noreply.github.com>
2024-05-01 21:43:50 +0000

    update to remove GC api key

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,16 +1,17 @@
-describe('empty spec', () => {
+describe("empty spec", () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
-  it('displays the resources text', () => {
-    cy.get('p')
-    .contains('This is a bare-bones Hugo project that has everything you need to quickly deploy it to Netlify');
-  })
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
-})
+    cy.visit("/");
+  });
+  it("displays the resources text", () => {
+    cy.get("p").contains(
+      "This is a bare-bones Hugo project that has everything you need to quickly deploy it to Netlify",
+    );
+  });
+  it("renders the image", () => {
+    cy.get("img")
+      .should("be.visible")
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+  });
+});
